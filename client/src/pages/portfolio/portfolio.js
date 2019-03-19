7fad63063f1a025bd61d28d72d80d75e62f66954
Jeremy Boggs <nicocchilover@gmail.com>
2019-03-19 17:19:13 -0400

    basic portfolio projects added

--- client/src/pages/portfolio/portfolio.js
+++ client/src/pages/portfolio/portfolio.js
@@ -2,9 +2,78 @@ import React, {Component} from 'react';
 import { Container, Button } from "reactstrap";
 import Gallery from "../../components/Gallery/Gallery";
 
+const projects = [
+    {
+        name: "Yazawa Nico",
+        headerImg: "https://i.ibb.co/Cbf6yZH/discord.jpg",
+        tech: "JavaScript, Node.js, Discord.js",
+        images: [
+            {
+                src:
+                    "https://i.ibb.co/Cbf6yZH/discord.jpg",
+                altText: "Slide 1",
+                caption: ""
+            },
+            {
+                src:
+                    "https://i.ibb.co/Cbf6yZH/discord.jpg",
+                altText: "Slide 2",
+                caption: ""
+            },
+            {
+                src:
+                    "https://i.ibb.co/Cbf6yZH/discord.jpg",
+                altText: "Slide 3",
+                caption: ""
+            }
+        ],
+        desc: "A multipurpose bot with fun commands and server moderation such as ban, kick, warn, and keep logs of user interactions.\n" +
+            "\n" +
+            "Built with JavaScript, Node.js and Discord.js API",
+        sub: "Discord Bot",
+        URL: "https://discordbots.org/bot/506839796921139203"
+    },
+    {
+        name: "ShopTrak",
+        headerImg: "https://i.ibb.co/cbbgYX1/shoptrak-header.png",
+        tech: "Node.js, Express.js, React, Redux, JavaScript",
+        images: [
+            {
+                src:
+                    "https://i.ibb.co/cbbgYX1/shoptrak-header.png",
+                altText: "Slide 1",
+                caption: ""
+            },
+            {
+                src:
+                    "https://i.ibb.co/cbbgYX1/shoptrak-header.png",
+                altText: "Slide 2",
+                caption: ""
+            },
+            {
+                src:
+                    "https://i.ibb.co/cbbgYX1/shoptrak-header.png",
+                altText: "Slide 3",
+                caption: ""
+            }
+        ],
+        desc: "React application to create groups of shopping lists to track who has purchased what items at what price.\n" +
+            "\n" +
+            "Built with JavaScript, Node.js and Express.js on the back-end, React, Redux on the front-end.",
+        sub: "Peer Guide Shopping",
+        URL: "http://labs10-shopping-list.netlify.com/"
+    }
+]
+
 class Portfolio extends Component {
     state = {
-        videoURL: 'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4'
+        formModal: false,
+    };
+
+    toggleModal = modalState => {
+        this.setState({
+            [modalState]: !this.state[modalState]
+        });
     };
 
     render() {
@@ -15,7 +84,7 @@ class Portfolio extends Component {
                     <hr style={{borderBottom: "2px solid #E31B6D", width: "5%", marginLeft: "47.5%", marginTop: "-10px"}} />
                 </div>
                 <Container style={{display: "flex", alignItems: "center", justifyContent: "center", marginTop: "5%"}}>
-                    <Gallery name="Projects" clickHandler={""} list={[{name: "Cras justo odio"},{name: "Cras justo odio"}, {name: "Dapibus ac facilisis in"}, {name: "Morbi leo risus"}, {name: "Porta ac consectetur ac"}, {name: "Vestibulum at eros"}]} />
+                    <Gallery open={this.state.formModal} toggleModal={this.toggleModal} name="Projects" clickHandler={""} list={projects} />
                 </Container>
             </div>
         );
