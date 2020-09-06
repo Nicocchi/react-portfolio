79f5ef1b5d035a523481b7b4d5a310597b23cb1e
Nicocchi <nicocchilover@gmail.com>
2020-09-05 23:39:30 -0400

    removed old portfolio files

--- src/App.js
+++ src/App.js
@@ -1,103 +1,26 @@
-import React, { Component } from 'react';
-import {Route, Switch, withRouter} from 'react-router-dom';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Home from "./pages/home/home";
-import NavBar from "./components/NavBar/NavBar";
-import Particles from "react-particles-js";
-import Portfolio from "./pages/portfolio/portfolio";
 
-import {
-    CSSTransition,
-    TransitionGroup,
-} from 'react-transition-group';
-import Contact from "./pages/contact/contact";
-import About from "./pages/about/about";
-import Blog from "./pages/blog/blog";
-import BlogPost from "./pages/blog/blogPost";
-
-const particleOpt = {
-    "particles": {
-        "number": {
-            "value": 160,
-            "density": {
-                "enable": false
-            }
-        },
-        "size": {
-            "value": 3,
-            "random": true,
-            "anim": {
-                "speed": 4,
-                "size_min": 0.3
-            }
-        },
-        "line_linked": {
-            "enable": false
-        },
-        "move": {
-            "random": true,
-            "speed": 1,
-            "direction": "top",
-            "out_mode": "out"
-        }
-    },
-    "interactivity": {
-        "events": {
-            "onhover": {
-                "enable": true,
-                "mode": "bubble"
-            },
-            "onclick": {
-                "enable": true,
-                "mode": "repulse"
-            }
-        },
-        "modes": {
-            "bubble": {
-                "distance": 50,
-                "duration": 2,
-                "size": 0,
-                "opacity": 0
-            },
-            "repulse": {
-                "distance": 400,
-                "duration": 4
-            }
-        }
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Particles
-              params={particleOpt}
-              style={{position: "absolute", left: "0", top: "0", width: "100%", height: "100%", zIndex: "-1 !important"}}
-          />
-          <NavBar history={this.props.history}/>
-          <Route render={({location}) => (
-              <TransitionGroup>
-                  <CSSTransition
-                      key={location.key}
-                      timeout={500}
-                      classNames="fade"
-                  >
-                      <Switch location={location}>
-                          <Route exact path='/' component={Home} />
-                          <Route path="/portfolio" component={Portfolio} />
-                          <Route path="/contact" component={Contact} />
-                          <Route path="/about" component={About} />
-                          <Route exact path="/blog" component={Blog} />
-                          <Route exact path="/blog/post/:id" render={props => <BlogPost {...props} />} />
-                      </Switch>
-                  </CSSTransition>
-              </TransitionGroup>
-          )}/>
-
-      </div>
-    );
-  }
-}
-
-export default withRouter(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,54 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-
-// Bootstrap, MU, fonts, other style libraries
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import "./assets/css/blk-design-system-react.css";
-import "./assets/css/nucleo-icons.css";
-import "./assets/scss/blk-design-system-react.scss?v=1.0.0";
-import "./assets/demo/demo.css";
-
-// Router
-import { BrowserRouter as Router } from "react-router-dom";
-
-// Redux
-import { createStore, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
-
-import * as serviceWorker from "./serviceWorker";
-
-import rootReducer from "./store/reducers/";
-
-// Redux dev tools
-const composeEnhancers =
-    typeof window === 'object' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-        }) : compose;
-
-const enhancer = composeEnhancers(
-    applyMiddleware(thunk, logger),
-    // other store enhancers if any
-);
-const store = createStore(rootReducer, enhancer);
-
-const rootElement = document.getElementById("root");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <App />
-        </Router>
-    </Provider>,
-    rootElement
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
